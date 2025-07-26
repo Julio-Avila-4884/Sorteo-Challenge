120a689747618c82d827370cb10c2c5f9fa2ffc7
Julio-Avila-4884 <julio_re_av@outlook.com>
2025-07-25 22:22:54 -0600

    se procede a perfeccionar el programa,  se agrega el aviso de cuando no se ingresa algun valor valido, y no lo registra en la lista., -se agrega intruccion <li> para generar una lista vertical en un formato <ul>

--- app.js
+++ app.js
@@ -20,15 +20,28 @@ function asignarTextoElemento(elemento, texto) {
 
 function agregarAmigo() {
 
-    nombre = document.getElementById("amigo").value; // siempre lleba el .value sin importar si es texto o numero
+    nombre = document.getElementById("amigo").value.trim(); // siempre lleba el .value sin importar si es texto o numero
+    // se valida que el nombre no este vacio
+    if (nombre === "") {
+        alert("Introduce un nombre válido.");
+        return;
+    }
+
+    // se registra el nombre en la lista
     listaNombres.push(nombre);
-   
+    const li = document.createElement("li");  // crea elemento <li> antes de agregarlo a la lista, lo que permite que se muestre en lista
+    li.textContent = `${listaNombres.length}. ${nombre}`; // asigna el nombre al contenido del elemento <li>
+    document.getElementById("listaAmigos").appendChild(li); // agrega el elemento <li> a la lista con el id "listaAmigos" que es <ul> la cual solo se imprime horizontalmente
+    // se limpia el campo de entrada
+    document.getElementById("amigo").value = ""; // limpia el campo de entrada      
+/* borrar 
     console.log(multiplicador1);
     console.log("dato introducido " + nombre);
     console.log("lista " + listaNombres);
     console.log("datos en lista " + listaNombres.length);
     console.log("fin ciclo")
-     
+     */
+
     return;
 }
 
@@ -39,19 +52,23 @@ function sortearAmigo() {
 // en caso de exceder los valores de la lista el inicial 10 se incrementara en 10 veces 
     while (multiplicador1 < listaNombres.length){ 
         multiplicador();
-        console.log("nuevo valor " + multiplicador1);
+        //console.log("nuevo valor " + multiplicador1);
         break;
     }
     // generar numero aleatorio
      numeroSorteado = Math.floor(Math.random()*multiplicador1);
+     /* borrar 
      console.log("iniciando")
      console.log("numero seleccionado: " + numeroSorteado);
      console.log("cantidad de datos: " + listaNombres.length)
-    // se agrega para evitar un bucle infinito de iteraciones cuando se terminan los numeros
+    */
+     // se agrega para evitar un bucle infinito de iteraciones cuando se terminan los numeros
         if (listaNumerosUsados.length == multiplicador1) {
+         asignarTextoElemento("ul", "Sorteo de amigos secretos finalizado " );
+
          console.log("SORTEO TERMINADO, NO ES POSIBLE TENER MAS GANADORES")
          console.log(listaNumerosUsados);
-    // SE DEBE DESHABILITAR EL VOTON "SORTEO"
+        // SE DEBE DESHABILITAR EL BOTON "SORTEO"
             }
     //si el numero ya fue usado, se sortea de nuevo
             else if (listaNumerosUsados.includes(numeroSorteado)){ 
@@ -63,8 +80,10 @@ function sortearAmigo() {
                     // si se detecta que es menor o igual al los datos introducidos,
                     // se imprime el ganador
                     if (numeroSorteado < listaNombres.length){ 
-                    console.log("hay ganador" );
-                    console.log("numero ganador: " +listaNombres[numeroSorteado]);
+                        asignarTextoElemento("ul", "amigo secreto: " + `${listaNombres[numeroSorteado]}`);
+                    
+                        console.log("hay ganador" );
+                        console.log("numero ganador: " +listaNombres[numeroSorteado]);
                     } else {
                         // si el numero es mayor se almacena
                         console.log("valor fuera del rango");
@@ -85,5 +104,3 @@ multiplicador1 *=10;
 return multiplicador1;
 
 }
-
-
