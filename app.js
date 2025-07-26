f4deab57edc14e69481cb5770471be6fdf23c6ea
Julio-Avila-4884 <julio_re_av@outlook.com>
2025-07-25 22:32:08 -0600

    se depura programa, se eliminan los console.log

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
 // Nombres
 let nombre;
 let listaNombres =[];
-
 // sorteo
 let numeroSorteado;
 let listaNumerosUsados =[];
 let multiplicador1=10;
-
 // Modificación de textos
-
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
 }
-
 //seccion de agregar información
-
 function agregarAmigo() {
-
     nombre = document.getElementById("amigo").value.trim(); // siempre lleba el .value sin importar si es texto o numero
     // se valida que el nombre no este vacio
     if (nombre === "") {
         alert("Introduce un nombre válido.");
         return;
     }
-
     // se registra el nombre en la lista
     listaNombres.push(nombre);
     const li = document.createElement("li");  // crea elemento <li> antes de agregarlo a la lista, lo que permite que se muestre en lista
@@ -34,18 +26,8 @@ function agregarAmigo() {
     document.getElementById("listaAmigos").appendChild(li); // agrega el elemento <li> a la lista con el id "listaAmigos" que es <ul> la cual solo se imprime horizontalmente
     // se limpia el campo de entrada
     document.getElementById("amigo").value = ""; // limpia el campo de entrada      
-/* borrar 
-    console.log(multiplicador1);
-    console.log("dato introducido " + nombre);
-    console.log("lista " + listaNombres);
-    console.log("datos en lista " + listaNombres.length);
-    console.log("fin ciclo")
-     */
-
     return;
 }
-
-
 // seleccion sortear 
 function sortearAmigo() {
     console.log("multiplicador inicial: " + multiplicador1);
@@ -57,11 +39,7 @@ function sortearAmigo() {
     }
     // generar numero aleatorio
      numeroSorteado = Math.floor(Math.random()*multiplicador1);
-     /* borrar 
-     console.log("iniciando")
-     console.log("numero seleccionado: " + numeroSorteado);
-     console.log("cantidad de datos: " + listaNombres.length)
-    */
+     
      // se agrega para evitar un bucle infinito de iteraciones cuando se terminan los numeros
         if (listaNumerosUsados.length == multiplicador1) {
             asignarTextoElemento("ul", "Sorteo de amigos secretos finalizado " );
@@ -92,16 +70,10 @@ function sortearAmigo() {
                         // se regrsa a la seccion sortear
                         sortearAmigo();     
                     }
-                }
-                        
+                }                    
 }
-
-
-
 // cambio de multiplicador en caso de ser mas valores al maximo
-
 function multiplicador(){
 multiplicador1 *=10;
 return multiplicador1;
-
 }
